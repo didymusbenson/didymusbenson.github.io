55c285555375755644b65d72a783c056825eb74e
Didymus <didymus.benson@gmail.com>
2017-09-13 10:27:31 -0600

    demodemo

--- cit261/examples/js/default.js
+++ cit261/examples/js/default.js
@@ -1,43 +1,50 @@
 document.getElementById("fluency").addEventListener("click", activate);
 
 function activate() {
-
     var e = document.getElementById("fluency");
-
-
     if (e.classList.contains('fluency-active')) {
-       e.classList.remove('fluency-active');
+        e.classList.remove('fluency-active');
         e.classList.add('fluency-inactive');
     } else {
         e.classList.remove('fluency-inactive');
         e.classList.add('fluency-active');
     }
-
-
 }
-
 var sauce = document.getElementById("sauce");
-var sourcejs = "./js/" + document.location.href.split("examples/")[1].split(".")[0] + ".js";
-var client = new XMLHttpRequest();
-client.open('GET', sourcejs);
-client.onreadystatechange = function() {
-    sauce.innerHTML = client.responseText;
+if (sauce) {
+    var sourcejs = "./js/" + document.location.href.split("examples/")[1].split(".")[0] + ".js";
+    var jsclient = new XMLHttpRequest();
+    jsclient.open('GET', sourcejs);
+    jsclient.onreadystatechange = function () {
+        sauce.innerHTML = jsclient.responseText;
+    }
+    jsclient.send();
 }
-client.send();
+var css_sauce = document.getElementById("css_sauce");
+if (css_sauce) {
+    var sourcecss = "./css/" + document.location.href.split("examples/")[1].split(".")[0] + ".css";
+    var cssclient = new XMLHttpRequest();
+    cssclient.open('GET', sourcecss);
+    cssclient.onreadystatechange = function () {
+        css_sauce.innerHTML = cssclient.responseText;
+    }
+    cssclient.send();
+}
 
-/****
-#fluency{
-    position:fixed;
-    top:15px;
-    left: -700px;
-    width:900px;
-    max-width:calc(900px - 1.5em);
-    transition: 0.2s ease-in-out;
+
+/************* Something Fun *******************/
+if ( window.addEventListener ) {
+    var konami_keys = [], konami_code = "38,38,40,40,37,39,37,39,66,65";
+    window.addEventListener("keydown", function(e){
+        konami_keys.push( e.keyCode );
+        if ( konami_keys.toString().indexOf( konami_code ) >= 0 ){
+        	awwwwyeeeeee();
+          konami_keys = [];
+            }
+    }, true);
 }
 
-#fluency:hover{
-    cursor:pointer;
-    left: -675px;
-    transition: 0.2s ease-in-out;
+function awwwwyeeeeee(){
+    var easter_egg_basket = document.getElementsByClassName("tool")[0];
+    easter_egg_basket.innerHTML = "<script></script><div class='hue'><audio id='mysound' src='https://upload.wikimedia.org/wikipedia/en/3/3f/Darude_-_Sandstorm.ogg'></audio><div class='bassdrop' onmouseover=\"document.getElementById('mysound').play()\" onmouseout=\"document.getElementById('mysound').pause()\"><img src='./img/hue.PNG'/></div></div><div class='rainbow'><h1 class='ooer'>OH THE HUE-MANATEE!</h1></div>";
 }
-***/
