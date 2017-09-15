3c2f136458228e3f4dc8056bf1a17e3827c86b11
Didymus <didymus.benson@gmail.com>
2017-09-15 12:21:57 -0600

    updated slideshow

--- cit261/tutorials/js/tutorial.js
+++ cit261/tutorials/js/tutorial.js
@@ -1,4 +1,6 @@
 document.getElementById("fluency").addEventListener("click", activate);
+document.querySelector("#arrowleft").addEventListener("click", back);
+document.querySelector("#arrowright").addEventListener("click", forward);
 
 function activate() {
     var e = document.getElementById("fluency");
@@ -13,17 +15,37 @@ function activate() {
 
 
 var slides = document.getElementsByTagName("section");
+var button_wrapper = document.querySelector("#control_buttons");
 for (var i = 0;i < slides.length; i++){
     var button = document.createElement("div");
     button.classList.add("control_button");
     button.dataset.slide = i;
     button.addEventListener("click", change_slides);
-    document.getElementById("control_buttons").appendChild(button);
+    button_wrapper.appendChild(button);
 }
 
 var buttons = document.getElementsByClassName("control_button");
 buttons[0].classList.add("active");
 
+
 function change_slides(){
-    console.log(this.dataset.slide);
+    console.log(this, this.dataset.slide);
+    activate_slide(this, slides[this.dataset.slide]);
+}
+
+function activate_slide(button, slide){
+    for (var i = 0; i < slides.length; i++){
+        slides[i].classList.remove("active")
+        slides[i].classList.add("inactive")
+        buttons[i].classList.remove("active");
+    }
+    slide.classList.remove("inactive");
+    slide.classList.add("active");
+    button.classList.add("active");
+}
+function forward(){
+    console.log("forward")
+}
+function back(){
+    console.log("back")
 }
