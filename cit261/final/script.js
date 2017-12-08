adb2c594ffca56d1640ff41c75d9e87ce5ec7d76
didymusbenson <didymus.benson@gmail.com>
2017-12-08 15:59:36 -0700

    Cleanup

--- cit261/final/script.js
+++ cit261/final/script.js
@@ -1,5 +1,3 @@
-
-
 // Add the movies to the page for browsing
 function make_ui(movies) {
     // Add a card for every movie
@@ -7,7 +5,7 @@ function make_ui(movies) {
         var stringified_movie = JSON.stringify(movie).replace(/\"/g, "'");
         console.log(stringified_movie);
         console.log(movie.genre);
-        var cards = document.querySelector("." + movie.genre );
+        var cards = document.querySelector("." + movie.genre);
         cards.innerHTML += "<div class='card'><div class='card-img' ><img  src='" + movie.poster + "'></div><div class='card-title' data-movie=\"" + stringified_movie + "\" data-action='watch'><h3 >" + movie.title + "</h3></div><div class='card-display'><p data-movie='" + movie.id + "' data-action='learn'><a href='#popup'>?</a></p><p data-movie=\"" + stringified_movie + "\" data-action='watch'>▶</p><p data-action='favorite' data-movie='" + movie.id + "' data-favorited='false'>☆</p></div></div>";
     });
     // MAYBE: add tabs for genres and a tab for favorites.
@@ -15,7 +13,7 @@ function make_ui(movies) {
     make_listeners();
 }
 
-function move_right(e){
+function move_right(e) {
     var style = window.getComputedStyle(e);
     var matrix = new WebKitCSSMatrix(style.webkitTransform);
     e.style.transform = "translateX(calc(" + matrix.m41 + " - 1px) )"
@@ -100,23 +98,23 @@ function favorite(el) {
 }
 
 // Change the tab on the genre selection area
-function changeTabs(genre){
+function changeTabs(genre) {
     var tabs = document.querySelectorAll(".film_nav h3");
     var tab_contents = document.querySelectorAll(".cards");
-    tabs.forEach(function(tab){
+    tabs.forEach(function (tab) {
         console.log(tab, genre);
-        if (tab.classList.contains("active_tab")){
+        if (tab.classList.contains("active_tab")) {
             tab.classList.remove("active_tab");
         }
-        if (tab.dataset.target == genre ){
+        if (tab.dataset.target == genre) {
             tab.classList.add("active_tab");
         }
     })
-    tab_contents.forEach(function(tab){
-        if (!tab.classList.contains("hidden")){
+    tab_contents.forEach(function (tab) {
+        if (!tab.classList.contains("hidden")) {
             tab.classList.add("hidden");
         }
-        if (tab.classList.contains(genre)){
+        if (tab.classList.contains(genre)) {
             tab.classList.remove("hidden");
         }
     })
@@ -152,11 +150,11 @@ function learn(movie) {
 // Add a movie to the theater element to be watched.
 function play_movie(movie) {
     //clear the theater first
-   var movie_obj = JSON.parse(movie.replace(/\'/g, '"'));
-   var theatre = document.querySelector(".theatre");
-   theatre.innerHTML = "<video width=\"100%\" controls autoplay type=\"video/mp4\"  data-movie=\"" + movie_obj.name + "\" src=\"" + movie_obj.video + "\"> Your browser does not support the video tag.  </video>";
+    var movie_obj = JSON.parse(movie.replace(/\'/g, '"'));
+    var theatre = document.querySelector(".theatre");
+    theatre.innerHTML = "<video width=\"100%\" controls autoplay type=\"video/mp4\"  data-movie=\"" + movie_obj.name + "\" src=\"" + movie_obj.video + "\"> Your browser does not support the video tag.  </video>";
     // Scroll to top of page to view movie.
-//    window.scrollTo(0, 0);
+    //    window.scrollTo(0, 0);
     document.querySelector(".theatre").scrollIntoView();
     // Create video tag in theater
 }
