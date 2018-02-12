e1dc0cefb876eececbe449f8ff432b3b94b368e2
Didymus <didymus.benson@gmail.com>
2018-02-12 09:06:47 -0700

    did this one work?

--- v2/scripts.js
+++ v2/scripts.js
@@ -1,12 +1,19 @@
 /* No checkbox hack today. */
 var about = document.querySelector(".about");
-about.addEventListener("click", toggleCollapse);
-about.addEventListener("touchstart", toggleCollapse);
-function toggleCollapse() {
+about.addEventListener("click", function() {
     if (about.classList.contains("collapse")) {
         about.classList.remove("collapse");
     } else {
         about.classList.add("collapse");
     }
-    ev.preventDefault();
-}
+    event.preventDefault();
+}, false);
+about.addEventListener("touchstart", function() {
+    if (about.classList.contains("collapse")) {
+        about.classList.remove("collapse");
+    } else {
+        about.classList.add("collapse");
+    }
+    event.preventDefault();
+}, false);
+
